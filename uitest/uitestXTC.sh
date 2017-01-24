ff30a6b6746f2cecc948402aed820cfd6f9cd36a
David Hathaway <david.hathaway@xamarin.com>
2017-01-23 19:10:40 -0800

    Basic submit sample for UITest

--- uitest/uitestXTC.sh
+++ uitest/uitestXTC.sh
@@ -1,2 +1,9 @@
-#uitestXTC.sh
-#TODO script here
+function section_print {
+    echo "\n=== $* ==="
+}
+
+# Parameter passed from the upstream job that built the iOS app
+cd "$UPSTREAM_WORKSPACE"
+
+# Upload Command
+"$MONO_PATH/bin/mono" $XTC_UPLOADER_PATH submit $XTC_APP_FILE $XTC_API_KEY --devices $XTC_DEVICE_KEY --series $XTC_SERIES --locale $XTC_LOCALE --app-name $XTC_APP_NAME --user $XTC_USER --assembly-dir $XTC_TEST_DIRECTORY --async
