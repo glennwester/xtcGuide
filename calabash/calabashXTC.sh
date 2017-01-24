7926b3c04cb5a66cf4e3c44c515dad00d6d9cc97
Glenn Wester <glennwester@glenns-mbp-3.guest.corp.microsoft.com>
2017-01-23 17:42:44 -0800

    sample script for calabash

--- calabash/calabashXTC.sh
+++ calabash/calabashXTC.sh
@@ -1,2 +1,29 @@
 #calabashXTC.sh
 #TODO script here
+
+function fail {
+echo "$*" >&2
+exit 1
+}
+
+function section_print {
+echo "\n=== $* ==="
+}
+
+echo "installing bundler"
+
+gem install bundler
+
+echo "finished installing bundler"
+
+echo "installing gems"
+
+bundle install
+
+echo "finished installing gems"
+
+echo "initiate upload to xamarin test cloud"
+
+test-cloud submit $XTC_APK_FILE $XTC_API_KEY --devices $XTC_DEVICE_KEY --series $XTC_SERIES --locale $XTC_LOCALE --user $XTC_USER
+
+echo "finshed uploading to xamarin test cloud"
